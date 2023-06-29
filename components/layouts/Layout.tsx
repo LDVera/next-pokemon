7a07125f0251a2438acb7aa9f1b2b4fbfded68da
diego <ldvera41@gmail.com>
2023-06-29 15:05:17 -0600

    subida a produccion

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -8,7 +8,10 @@ interface Props {
   pokemon? :  string
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin
+
 export const Layout : FC<Props>  = ( {children, title, pokemon} ) => {
+
   return (
     <>
     
@@ -16,7 +19,11 @@ export const Layout : FC<Props>  = ( {children, title, pokemon} ) => {
         <title>{title}</title>
         <meta name="author" content="Diego" />
         <meta name="description" content={`Información sobre el Pokémon ${pokemon}`} />
-        <meta name="ketwords" content="XXXX, pokemon, pokedex" />
+        <meta name="ketwords" content={`${title},, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`inf Pokemon ${title}`}/>
+        <meta property="og:description" content={`pokemon ${title}`} />
+        <meta property="og:image" content={`${ origin }/img/banner.png`} />
       </Head>
 
       <Navbar/>
